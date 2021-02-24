4c33cacb92d02864f1a98774459a226c649a08c4
SAFA <safa.naoui25@gmail.com>
2021-02-24 15:50:57 +0100

    add navigation Bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListTeamsComponent } from './list-teams/list-teams.component';
+import { ListPostsComponent } from './list-posts/list-posts.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "",
+    component: ListTeamsComponent
+  },
+  {
+    path: "ListTeams",
+    component: ListTeamsComponent
+  },
+  {
+    path: "ListPosts",
+    component: ListPostsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
